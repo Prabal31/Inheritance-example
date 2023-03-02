922b3d81d60e6cc3a6198ccd56cf229f52f95b11
Prabal Manchanda <manchandaprabal878@gmial.com>
2023-03-01 22:24:13 -0500

    Added compare method which returns -1 1 or 0

--- src/a2/manchapr/Animal.java
+++ src/a2/manchapr/Animal.java
@@ -1,31 +1,35 @@
 package a2.manchapr;
 
-public abstract class Animal implements Cloneable {
+public abstract class Animal implements Comparable<Animal>, Cloneable {
+    
     double weight;
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
+    
     public double getWeight() {
         return weight;
     }
-
+    
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    
     public abstract String sound();
-
+    
     @Override
-    public Object clone() {
-        
-        try {
-            
-            return (Animal)super.clone();
-        }
-        
-        catch(CloneNotSupportedException e) {
-            
-           return("CloneNotSupportedException");
+    public int compareTo(Animal other) {
+        if (this.weight < other.weight) {
+            return -1;
+        } else if (this.weight > other.weight) {
+            return 1;
+        } else {
+            return 0;
         }
     }
+    
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+    
 }
 class Tiger extends Animal {
     
