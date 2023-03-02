8f0db0949f4c58e77704002270bb3c440cba289d
Prabal Manchanda <manchandaprabal878@gmial.com>
2023-03-02 13:04:38 -0500

    made variable private

--- src/a2/manchapr/Point.java
+++ src/a2/manchapr/Point.java
@@ -1,5 +1,26 @@
 package a2.manchapr;
 
 public class Point {
+    private double w,z;
+
+    public void setW(double w) {
+        this.w = w;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public double getW() {
+        return w;
+    }
+
+    public double getZ() {
+        return z;
+    }
+    Point() {
+        w=0;
+        z=0;
+    }
     
 }
