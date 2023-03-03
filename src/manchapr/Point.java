320aefd4a47240357c59cf686ca4bacd2cb2fffd
Prabal Manchanda <manchandaprabal878@gmial.com>
2023-03-03 14:29:21 -0500

    changed compare method

--- src/manchapr/Point.java
+++ src/manchapr/Point.java
@@ -32,12 +32,14 @@ public class Point implements Comparable<Point>, Cloneable{
     
     //default constructor to se the value of w and z to zero
     Point() {
+        
         w=0;
         z=0;
     }
     
     //parameter constructor to initialize the value of w and z
-    Point(double w, double z) {  
+    Point(double w, double z) { 
+        
         this.w=w;
         this.z=z;
     }
@@ -45,47 +47,63 @@ public class Point implements Comparable<Point>, Cloneable{
     //override compare to method which compare the two objects
     @Override
     public int compareTo(Point obj) {
-        if(this.w>obj.getW()&&this.z>obj.getZ()) {
+        
+        if(this.w>obj.getW()) {
+            
             return 1;
         } 
-        else if(this.w<obj.getW()&&this.z<obj.getZ()) {
+        
+        else if(this.z>obj.getZ()) {
+            
             return -1;
         }
+        
         else {
-            return 2;
+            
+            return 0;
         }
+        
     }
     
     //override equals method which chech if the two objects are equal or not deeply
+
     @Override
     public boolean equals(Object obj) {
+        
         if (this == obj) {
+            
             return true;
         }
         if (obj == null) {
+            
             return false;
         }
         if (getClass() != obj.getClass()) {
+            
             return false;
         }
         final Point other = (Point) obj;
+        
         if (Double.doubleToLongBits(this.w) != Double.doubleToLongBits(other.w)) {
+            
             return false;
         }
+        
         return Double.doubleToLongBits(this.z) == Double.doubleToLongBits(other.z);
     }
+    
    
     //override clone method to clone the object
     @Override
     public Object clone() throws CloneNotSupportedException {  
         
-        return super.clone();
-        
+        return super.clone();  
     }
     
     //to string metthod to print the value 
     @Override
     public String toString() {
+        
         return "[" + this.w + ", " + this.z + "]";
     }
     
