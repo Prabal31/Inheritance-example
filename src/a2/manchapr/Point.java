616c870fa285f28518b44694ba412cb97e791837
Prabal Manchanda <manchandaprabal878@gmial.com>
2023-03-03 01:35:12 -0500

    added compareto equals and clone method to the class

--- src/a2/manchapr/Point.java
+++ src/a2/manchapr/Point.java
@@ -1,26 +1,64 @@
 package a2.manchapr;
 
-public class Point {
+public class Point implements Comparable<Point>, Cloneable{
+    
     private double w,z;
 
-    public void setW(double w) {
+    public void setW(int w) {
+        
         this.w = w;
     }
 
-    public void setZ(double z) {
+    public void setZ(int z) {
+        
         this.z = z;
     }
 
     public double getW() {
+        
         return w;
     }
 
     public double getZ() {
+        
         return z;
     }
     Point() {
-        w=0;
-        z=0;
+        this.w=0;
+        this.z=0;
     }
     
+    Point(double w, double z) {  
+        this.w=w;
+        this.z=z;
+    }
+    
+    @Override
+    public int compareTo(Point obj) {
+        if(this==obj) {
+            return 1;
+        }
+        else {
+            return 0;
+        } 
+    }
+    @Override
+    public boolean equals(Object po) {
+        if(po==this) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    @Override
+    public Object clone() throws CloneNotSupportedException {  
+        
+        return super.clone();
+        
+    }
+    @Override
+    public String toString() {
+        return "[" + this.w + ", " + this.z + "]";
+    }
 }
